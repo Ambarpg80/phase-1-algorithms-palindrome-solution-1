ff920a3d3608be1d6d10d932649fe0666f16b6eb
Ambar <Ambarpg80@gmail.com>
2022-10-07 10:44:32 -0400

    commit tree 98b60e979afc0f93e28d9dca4b9fc9523a003a2c

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  //Write your algorithm here
+  // const wordArray = word.split('');
+  // const reversedWordArray = wordArray.reverse();
+  // const reversedWord = reversedWordArray.join('');
+  //  return word === reversedWord
+  return word === word.split('').reverse().join('');
+
 }
 
 /* 
   Add your pseudocode here
+  //If the word is the same in reverse I should return true.
+
+//Reverse the input string
+//If the input is the same as the reversed input
+   //return true
+ //else 
+  // return false
 */
 
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here 
+  Write a function that results in a boolean return after checking if a word is the same spelled forwards as it is spelled backwards.
 */
 
 // You can run `node index.js` to view these console logs
@@ -15,6 +29,16 @@ if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+  
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("tambor"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("jffj"));
 
   console.log("");
 
